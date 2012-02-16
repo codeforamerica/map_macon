31dd8ec13016b0a870c83c9b3ed5c8ead2b18980
Zach Williams <hey@zachwill.com>
2012-02-16 17:51:42 -0500

    Add leaflet

---
+++ public/js/map.js
@@ -0,0 +1,7 @@
+var map = new L.Map('map', {
+  center: new L.LatLng(32.8138, -83.6925),
+  layers: new L.TileLayer('http://a.tiles.mapbox.com/v3/zachwill.bibb/{z}/{x}/{y}.png'),
+  minZoom: 11,
+  maxZoom: 17,
+  zoom: 11
+});
